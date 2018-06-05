3649e8da04838e31bde1f7864898596fbb4782bd
feige1010 <gedawei163@163.com>
2018-06-05 17:14:37 +0800

    Revert "Revert "Revert "初始化项目"""
    This reverts commit 71c2f0667df549728c7e4f81bd087546672174c6.

--- 数据移植/代码/上线准备脚本/ZHIJIANG/shell/TEMP_LAST_TRANS_USER.sql
+++
@@ -1,9 +0,0 @@
--- 数据移植使用，存储选定经办角色操作员用户信息，供修改历史交易记录录入人信息
-drop table TEMP_LAST_TRANS_USER;
-create table TEMP_LAST_TRANS_USER
-(
-  cstno          VARCHAR2(20) not null,
-  userno         VARCHAR2(32) not null,
-  username       VARCHAR2(60)
-);
-
